3377d7ca572f8d352e628610ccef1edbd612805b
Mateus Moller Erkmann <erkmann08@gmail.com>
2021-04-20 12:21:37 -0300

    app configuration, lesson 14

---
+++ vendas/src/main/java/io/github/erkmann/repository/ClientesRepository.java
@@ -0,0 +1,10 @@
+package io.github.erkmann.repository;
+
+import io.github.erkmann.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+    }
+}
---
+++ vendas/src/main/java/io/github/erkmann/service/ClientesService.java
@@ -0,0 +1,26 @@
+package io.github.erkmann.service;
+
+import io.github.erkmann.model.Cliente;
+import io.github.erkmann.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientesService {
+
+    private ClientesRepository repository;
+
+    @Autowired
+    public ClientesService(ClientesRepository repository) {
+        this.repository = repository;
+    }
+
+    public void salvarCliente(Cliente cliente) {
+        validarCliente(cliente);
+        this.repository.persistir(cliente);
+    }
+
+    public void validarCliente(Cliente cliente) {
+
+    }
+}
