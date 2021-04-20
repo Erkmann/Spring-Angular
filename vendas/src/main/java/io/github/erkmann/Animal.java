649c1df2d36943f92bf3b75066fe97cc36537ccb
Mateus Moller Erkmann <erkmann08@gmail.com>
2021-04-20 20:39:50 -0300

    Aula 15

---
+++ vendas/src/main/java/io/github/erkmann/Animal.java
@@ -0,0 +1,5 @@
+package io.github.erkmann;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- vendas/src/main/java/io/github/erkmann/MinhaConfiguration.java
+++ vendas/src/main/java/io/github/erkmann/MinhaConfiguration.java
@@ -1,8 +1,15 @@
 package io.github.erkmann;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Development
 public class MinhaConfiguration {
+
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO A CONFIGURACAO DE DESENVOLVIMENTO");
+        };
+    }
 }
