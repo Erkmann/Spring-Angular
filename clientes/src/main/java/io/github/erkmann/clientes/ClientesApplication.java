dc790060ccce4cbccad2e13e0ceead2e1e5bfbd5
Erkmann <erkmann08@gmail.com>
2021-04-22 23:44:39 -0300

    34 end of first backend project

--- clientes/src/main/java/io/github/erkmann/clientes/ClientesApplication.java
+++ clientes/src/main/java/io/github/erkmann/clientes/ClientesApplication.java
@@ -1,7 +1,13 @@
 package io.github.erkmann.clientes;
 
+import io.github.erkmann.clientes.model.entity.Cliente;
+import io.github.erkmann.clientes.model.repository.ClienteRepository;
+import lombok.Builder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
