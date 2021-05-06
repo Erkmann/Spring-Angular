bc6068a261019d424cf81c24f97fa80132886de1
Erkmann <erkmann08@gmail.com>
2021-05-05 22:05:05 -0300

    jQuery fixed, clientes module created

--- clientes-app/src/app/app.component.ts
+++ clientes-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { jQuery } from 'jquery';
 import { Component, AfterViewInit } from '@angular/core';
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,22 +10,16 @@ export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
 
   ngAfterViewInit() {
-    (function ($) {
-      'use strict';
+    var path = location.pathname;
 
-      // Add active state to sidbar nav links
-      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-      $('#layoutSidenav_nav .sb-sidenav a.nav-link').each( () => {
-        if (this.href === path) {
-          $(this).addClass('active');
-        }
-      });
+    $('#layoutSidenav_nav .sb-sidenav a.nav-link').each((index: any, element: any) => {
+      if ($(element).attr('href') === path) {
+        $(element).addClass("active");
+      }
+    });
 
-      // Toggle the side navigation
-      $('#sidebarToggle').on('click', function (e) {
-        e.preventDefault();
-        $('body').toggleClass('sb-sidenav-toggled');
-      });
-    })(jQuery);
+    $("#sidebarToggle").on("click", function (event: { preventDefault: () => void; }) {
+      $("body").toggleClass("sb-sidenav-toggled");
+    });
   }
 }
